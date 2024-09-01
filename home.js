8ce2842bd4be54a05a4f3dd6b4503c876ea624cb
Isabela Santiago <isadelga2009@gmail.com>
2024-09-01 20:58:04 -0300

    Update home.js

--- home.js
+++ home.js
@@ -3,11 +3,11 @@ const img = document.querySelectorAll("#img img");
 
 let idx = 0;
 
-function carrossel(){
+function carrossel (){
     idx++;
 
-    if (idx > img.length - 1) {
-        idx = 0;
+    if (idx>img.length-1) {
+        idx=0;
     }
 
     imgs.style.transform = `translateX(${-idx * 500}px)`;
